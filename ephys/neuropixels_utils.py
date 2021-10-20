202f47dcf7deb966853af795a99c91f437d82c70
bram nuttin <bramnuttin@hotmail.com>
2021-10-20 12:13:20 +0200

    CSD estimation prototype

--- ephys/neuropixels_utils.py
+++ ephys/neuropixels_utils.py
@@ -1,4 +1,4 @@
-from ephys.readSGLX import readMeta, SampRate, makeMemMapRaw, ExtractDigital
+from ephys.readSGLX import readMeta, SampRate, makeMemMapRaw, ExtractDigital, GainCorrectIM, GainCorrectNI, ChannelCountsNI
 import numpy as np
 
 
@@ -15,6 +15,33 @@ def extract_sync(file_path):
     # get digital data for the selected lines
     digArray = ExtractDigital(rawData, firstSamp, lastSamp, dw, dLineList, meta)
     return digArray
+
+def extract_analog(file_path, channels):
+    # For analog channels: zero-based index of a channel to extract,
+    # gain correct and plot (plots first channel only)
+    chanList = channels
+
+    # Read in metadata; returns a dictionary with string for values
+    meta = readMeta(file_path)
+
+    # parameters common to NI and imec data
+    sRate = SampRate(meta)
+    firstSamp = 0
+    lastSamp = int(sRate * float(meta['fileTimeSecs'])) - 1
+
+    rawData = makeMemMapRaw(file_path, meta)
+
+    selectData = rawData[chanList, firstSamp:lastSamp+1]
+    if meta['typeThis'] == 'imec':
+        # apply gain correction and convert to uV
+        convData = 1e6*GainCorrectIM(selectData, chanList, meta)
+    else:
+        MN, MA, XA, DW = ChannelCountsNI(meta)
+        # print("NI channel counts: %d, %d, %d, %d" % (MN, MA, XA, DW))
+        # apply gain coorection and conver to mV
+        convData = 1e3*GainCorrectNI(selectData, chanList, meta)
+    return convData
+   
 
 
 def get_trialgroup_stimulus_info(sync_trace):
